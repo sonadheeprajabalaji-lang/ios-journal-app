7e2813790d3acf7c1900ad3e2f7d49516a356463
sonadheeprajabalaji-lang <sonadheep.rajabalaji@student.uts.edu.au>
2026-05-04 15:08:06 +1000

    default

---
+++ Swift/SnippetsJournal/SnippetsJournal/SnippetsJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnippetsJournalApp.swift
+//  SnippetsJournal
+//
+//  Created by Admin on 04/05/26.
+//
+
+import SwiftUI
+
+@main
+struct SnippetsJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
